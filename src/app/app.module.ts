866d8ee5ca6c78d125beb79959b84901a327c000
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-08-12 14:37:26 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
